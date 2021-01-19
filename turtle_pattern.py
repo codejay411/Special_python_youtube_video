f144af475bb7513540d4cad30fbe0de97a3550ab
Jay Prakash Bind <60916627+codejay411@users.noreply.github.com>
2021-01-19 19:09:38 +0530

    Add files via upload

---
+++ turtle_pattern.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+speed(0)
+bgcolor('black')
+pensize(3)
+
+for i in range(5):
+    for j in ["red", "orange", "green", "yellow", "white", "blue", "cyan"]:
+        color(j)
+        left(12)
+        for k in range(4):
+            forward(200)
+            left(90)
+hideturtle()
+done()
