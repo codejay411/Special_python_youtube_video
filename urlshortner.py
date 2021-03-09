403423437adfec477258e385ee32abbc2404e0cf
Jay Prakash Bind <60916627+codejay411@users.noreply.github.com>
2021-03-09 09:15:54 +0530

    Add files via upload

---
+++ urlshortner.py
@@ -0,0 +1,9 @@
+import pyshorteners
+
+s = pyshorteners.Shortener()
+
+link = s.tinyurl.short("https://www.linkedin.com/in/jay-prakash-bind-b0736217a/")
+
+print(link)
+
+# https://tinyurl.com/ya9cfzqq  
